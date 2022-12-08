e5525fc1da16512012ed853d4ff59af42fd65829
NyaPPuu <mnyappu@gmail.com>
2022-12-08 11:32:55 +0900

    example update

--- src/routes/_.tsx
+++ src/routes/_.tsx
@@ -1,4 +1,7 @@
-import app from "../lib/common";
+import { Button } from "@mui/material";
+import React from "react";
+import app, { render } from "../lib/common";
+import { cache } from "../lib/theme";
 
 console.log("routes/_.tsx", sauron);
 
@@ -7,13 +10,16 @@ app.storage.sync.get(["rainbowBackground"], function(options: { [key: string]: a
 		sauron("body", () => {
 			document.body.style.transition = "background-color 1s";
 			let colorIdx = 0;
-			setInterval(() => {
+			const interval = setInterval(() => {
 				document.body.style.backgroundColor = "hsl(" + (360 * colorIdx / 60) + ",80%,50%)";
 				colorIdx++;
 				if (colorIdx == 60) {
 					colorIdx = 0;
 				}
 			}, 1000);
+
+			document.body.append(render(<Button variant="contained" onClick={() => { clearInterval(interval); }}>Wow! rainbow!</Button>, cache()));
 		});
 	}
 });
+
